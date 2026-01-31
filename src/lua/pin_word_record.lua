68126432f2e60e7cb5ab25d595dce8778d342d67
Passthem <passthem183@gmail.com>
2026-01-31 20:22:44 +0800

    +什么

--- src/lua/pin_word_record.lua
+++ src/lua/pin_word_record.lua
@@ -1,8 +1,8 @@
 local pin_word_records =
-{
-	["xihr"] = { "喜欢", },
-	["ba"] = { "吧", "把", },
+{ 	["xihr"] = { "喜欢", },
+	["ufme"] = { "什么", },
 	["vidk"] = { "置顶", },
+	["ba"] = { "吧", "把", },
 	["sudu"] = { "速度", },
 }
 return pin_word_records
